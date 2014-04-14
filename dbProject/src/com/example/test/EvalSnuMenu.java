5e7b3226787dcc42ddba703d831729ccdab780da
yujinee <yujinee@yujinee-PC>
2014-04-14 18:38:22 +0900

    ccc

---
+++ dbProject/src/com/example/test/EvalSnuMenu.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EvalSnuMenu extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.evalsnumenu_layout);
+		
+	}
+}
